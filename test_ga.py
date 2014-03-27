ba600ef0c5a9060710ada30a11a86805478840ae
Joseph Yesselman <jyesselm@umich.edu>
2014-03-26 22:43:21 -0700

    tests with SVM and trying to evolve correlations wiht GA

--- test_ga.py
+++ test_ga.py
@@ -3,6 +3,7 @@ from pyevolve import Mutators, Initializators
 from pyevolve import GSimpleGA, Consts
 import matplotlib.pyplot as plt
 import numpy as np
+import math
 
 import cPickle as pickle
 
@@ -12,16 +13,32 @@ train_data = pickle.load( open( "train_data.p", "rb" ) )
 # This is the Sphere Function
 def sphere(xlist):
 	total = 0
+	sum_x = 0
+	sum_y = 0
+	sum_xy = 0
+	sum_x2 = 0
+	sum_y2 = 0
 	for data in train_data:
 		score = 0
 		for j,weight in enumerate(xlist):
 			score += weight*data[j]
+		#total += abs(score - data[-1])**2
+		sum_x += data[-1]
+		sum_y += score
+		sum_xy += data[-1]*score
+		sum_x2 += data[-1]**2
+		sum_y2 += score**2
 
-		if data[-1] > 99.99 and score > 99.99:
-			continue
+	n = float(len(train_data))
 
-		total += abs(score - data[-1])**2
-	return total
+
+	#print sum_x,sum_y,sum_xy,sum_x2,sum_y2
+	try:
+		r = (sum_xy - (sum_x*sum_y)/n) / math.sqrt((sum_x2 - ((sum_x**2)/n))*(sum_y2 - ((sum_y**2)/n)))
+	except:
+		r = 0
+
+	return abs(r)
 
 def test_data(best):
 	x = []
@@ -36,24 +53,24 @@ def test_data(best):
 		y.append(score)
 
 
-		if data[-1] > 99.9:
-			print score,data[-1],data
+		#if data[-1] > 99.9:
+		#	print score,data[-1],data
 
 	return np.array(x),np.array(y)
 
 def run_main():
 	genome = G1DList.G1DList(5)
-	genome.setParams(rangemin=-10, rangemax=50, bestrawscore=0.00, rounddecimal=2)
+	genome.setParams(rangemin=0, rangemax=30, bestrawscore=0.00, rounddecimal=6)
 	genome.initializator.set(Initializators.G1DListInitializatorReal)
 	genome.mutator.set(Mutators.G1DListMutatorRealGaussian)
 	genome.evaluator.set(sphere)
 
 	ga = GSimpleGA.GSimpleGA(genome)
-	ga.setMinimax(Consts.minimaxType["minimize"])
-	ga.setGenerations(200)
+	ga.setMinimax(Consts.minimaxType["maximize"])
+	ga.setGenerations(20)
 	ga.setMutationRate(0.06)
 	ga.setCrossoverRate(0.9)
-	ga.setPopulationSize(100)
+	ga.setPopulationSize(200)
 	ga.evolve(freq_stats=1)
 
 	best = ga.bestIndividual()
---
+++ test_svm.py
@@ -0,0 +1,93 @@
+import numpy as np
+import cPickle as pickle
+import sys
+import matplotlib.pyplot as plt
+
+print(__doc__)
+
+def score_all(train_data,svr):
+	
+	x = []
+	y = []
+	for data in train_data:
+		if len(data) != 6:
+			continue
+		real = data.pop()
+		predicted = svr.predict(data)
+		x.append(real)
+		y.append(predicted)
+	return np.array(x),np.array(y)
+
+def score_orig(train_data,svr):
+	
+	x = []
+	y = []
+	for data in train_data:
+		predicted = svr.predict(data)
+		y.append(predicted)
+	return np.array(y)
+
+###############################################################################
+# Generate sample data
+
+train_data = pickle.load( open( "train_data.p", "rb" ) )
+test_data = pickle.load(  open( "test_data.p", "rb" ) )
+
+print len(train_data)
+X = train_data
+print len(X)
+y = []
+scores = []
+bin = 5
+for data in X:
+	score = data.pop()
+
+	binned = round (score / bin)
+	y.append(binned)
+	scores.append(score)
+y = np.array(y)
+
+###############################################################################
+# Fit regression model
+from sklearn import svm
+
+clf = svm.SVC()
+clf.fit(X,y)
+
+#svr_rbf = SVR(kernel='rbf', C=1e3, gamma=0.1)
+#svr_lin = SVR(kernel='linear', C=1e3)
+#svr_poly = SVR(kernel='poly', C=1e3, degree=2)
+#y_rbf = svr_rbf.fit(X, y).predict(X)
+#y_lin = svr_lin.fit(X, y).predict(X)
+#y_poly = svr_poly.fit(X, y).predict(X)
+
+#print X
+
+new_y= score_orig(X,clf)
+plt.subplot(121)
+plt.scatter(scores,new_y)
+
+x,y = score_all(test_data,clf)
+
+plt.subplot(122)
+plt.scatter(x,y)
+
+
+plt.show()
+
+
+sys.exit()
+
+###############################################################################
+# look at the results
+import pylab as pl
+pl.scatter(X, y, c='k', label='data')
+pl.hold('on')
+pl.plot(X, y_rbf, c='g', label='RBF model')
+pl.plot(X, y_lin, c='r', label='Linear model')
+pl.plot(X, y_poly, c='b', label='Polynomial model')
+pl.xlabel('data')
+pl.ylabel('target')
+pl.title('Support Vector Regression')
+pl.legend()
+pl.show()
